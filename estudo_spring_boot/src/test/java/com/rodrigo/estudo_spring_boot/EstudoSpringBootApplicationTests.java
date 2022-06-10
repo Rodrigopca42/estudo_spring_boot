a48f8f07b7307c1681f502c3a8cdcec6a74bbf5d
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-06-10 14:19:03 -0300

    readme
    readme

---
+++ estudo_spring_boot/src/test/java/com/rodrigo/estudo_spring_boot/EstudoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.estudo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
