6072fae6c974368e8736502ace96ef140c2732b3
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-23 16:17:08 -0300

    readme
    readme

--- estudo_spring_boot/src/main/java/com/rodrigo/estudo_spring_boot/EstudoSpringBootApplication.java
+++ estudo_spring_boot/src/main/java/com/rodrigo/estudo_spring_boot/EstudoSpringBootApplication.java
@@ -2,9 +2,17 @@ package com.rodrigo.estudo_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EstudoSpringBootApplication {
+
+	@GetMapping ("/Ola")
+	public String olaMundo(){
+		return "Ola ,Mundo!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringBootApplication.class, args);
